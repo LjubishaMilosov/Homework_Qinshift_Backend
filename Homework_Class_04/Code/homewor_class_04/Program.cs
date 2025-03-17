cdd8a9399eb4a951687a91e4a73af407f070b56f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-18 02:26:30 +0800

    Homework_class-04

---
+++ Homework_Class_04/Code/homewor_class_04/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Enter your birthday (yyyy-mm-dd):");
+string input = Console.ReadLine();
+if (DateTime.TryParse(input, out DateTime birthday))
+{
+    int age = AgeCalculator(birthday);
+    Console.WriteLine($"Your age is: {age}");
+}
+else
+{
+    Console.WriteLine("Invalid date format.");
+}
+
+
+static int AgeCalculator(DateTime birthday)
+{
+    DateTime today = DateTime.Today;
+    int age = today.Year - birthday.Year;
+
+    if (birthday.Date > today.AddYears(-age))
+    {
+        age--;
+    }
+
+    return age;
+}
+
+
+
+
