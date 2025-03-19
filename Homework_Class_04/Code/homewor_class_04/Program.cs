cd01af6d1fc42dfeb1a9aef63c2f59236cd8adbd
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-20 05:53:54 +0800

    ageCalculator-final refactor

--- Homework_Class_04/Code/homewor_class_04/Program.cs
+++ Homework_Class_04/Code/homewor_class_04/Program.cs
@@ -5,7 +5,47 @@
 //Note: take into consideration if the birthday is today, after or before today
 
 
-while (true)
+//while (true)
+//{
+//    Console.WriteLine("Enter your birthday (yyyy-mm-dd):");
+//    string input = Console.ReadLine();
+//    if (DateTime.TryParse(input, out DateTime birthday))
+//    {
+//        if (birthday > DateTime.Today)
+//        {
+//            Console.WriteLine("The date entered is in the future. Please enter a valid date.");
+//        }
+//        else
+//        {
+//            int age = AgeCalculator(birthday);
+//            Console.WriteLine($"Your age is: {age}");
+//            break;
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Invalid date format. Please try again.");
+//    }
+//}
+
+
+//static int AgeCalculator(DateTime birthday)
+//{
+//    DateTime today = DateTime.Today;
+//    int age = today.Year - birthday.Year;
+
+//    if (birthday.Date > today.AddYears(-age))
+//    {
+//        age--;
+//    }
+
+//    return age;
+//}
+
+
+// another way:
+
+     bool ageCalculatorInterface()
 {
     Console.WriteLine("Enter your birthday (yyyy-mm-dd):");
     string input = Console.ReadLine();
@@ -14,20 +54,21 @@ while (true)
         if (birthday > DateTime.Today)
         {
             Console.WriteLine("The date entered is in the future. Please enter a valid date.");
+            return false;
         }
         else
         {
             int age = AgeCalculator(birthday);
             Console.WriteLine($"Your age is: {age}");
-            break;
+            return true;
         }
     }
     else
     {
         Console.WriteLine("Invalid date format. Please try again.");
+        return false;
     }
 }
-
 
 static int AgeCalculator(DateTime birthday)
 {
@@ -42,6 +83,7 @@ static int AgeCalculator(DateTime birthday)
     return age;
 }
 
+while (!ageCalculatorInterface());
 Console.ReadLine(); 
 
 
