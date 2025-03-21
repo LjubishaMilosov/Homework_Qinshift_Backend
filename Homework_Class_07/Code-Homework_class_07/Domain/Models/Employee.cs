7f7bf38525f3624d9e69dfc08fd6c2d9cc23124c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-21 20:39:39 +0800

    homework_class07-part1

---
+++ Homework_Class_07/Code-Homework_class_07/Domain/Models/Employee.cs
@@ -0,0 +1,44 @@
+﻿
+
+using Domain.Enums;
+
+namespace Domain.Models
+{
+
+// EXERCISE 1
+//Create a class library project and in it create all classes
+//● Create a class called Employee with the following
+//properties:
+//▹ FirstName
+//▹ LastName
+//▹ Role - Enum(Sales, Manager, Other )
+//▹ Salary - protected, double
+//● Create two methods:
+//▹ PrintInfo - Prints FirstName, LastName and Salary
+//▹ GetSalary - returns the salary
+
+
+    public class Employee
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public RoleEnum Role { get; set; }
+        protected double Salary { get; set; }
+        public Employee() { }
+        public Employee(string firstname, string lastName, RoleEnum role, double salary)
+        {
+            Firstname = firstname;
+            LastName = lastName;
+            Role = role;
+            Salary = salary;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"First Name: {Firstname}, Last Name: {LastName}, Salary: {Salary}");
+        }
+        public double GetSalary()
+        {
+            return Salary;
+        }
+    }
+}
